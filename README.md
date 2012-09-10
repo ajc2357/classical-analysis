7f3a4dbc452a118e21f87191d5f3d1a4bd3f61cd
Carl Hall <carl@hallwaytech.com>
2012-09-09 23:39:13 -0700

    Update processing to read date range from allclassical.org and store in Redis.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Run `run.rb` in the root of the project.
 
 ## Contributing
 
--- lib/classical_analysis.rb
+++ lib/classical_analysis.rb
@@ -1,25 +1,32 @@
-require "classical_analysis/version"
+require 'classical_analysis/version'
 require 'nokogiri'
+require 'open-uri'
+require 'redis'
 
 module ClassicalAnalysis
   class AllClassicalDoc
     def self.run
-      entries = []
-      # http://www.allclassical.org/assets/playlist/playlistFormat.php?t=0.7356784949079156&day=2012-09-01
-      doc = Nokogiri::HTML(open('playlistFormat.html'))
-      doc.css('ul').each do |ul|
-        entry = {}
-        ul.children.each do |li|
-          li.children.each do |span|
-            if !span['class'].nil? && span['class'] != 'pl_buynow'
-              entry[span['class'][3..-1]] = span.text
+      db = Redis.new(:host => '127.0.0.1', :port => 6379)
+      9.times do |day|
+        date = "2012-09-%02d" % [day + 1]
+        puts "Check #{date}"
+        # t=0.7356784949079156&
+        url = "http://www.allclassical.org/assets/playlist/playlistFormat.php?day=#{date}"
+        doc = Nokogiri::HTML(open(url))
+        doc.css('ul').each do |ul|
+          entry = {}
+          ul.children.each do |li|
+            if !li.children.empty?
+              span = li.first_element_child
+              if !span['class'].nil? && span['class'] != 'pl_buynow'
+                entry[span['class'][3..-1]] = span.text
+              end
             end
           end
+          # store data in redis
+          db.hmset "#{date} #{entry['time']}", *entry.flatten
         end
-        entries << entry
-        # TODO store in redis
       end
-      puts entries.length
     end
   end
 end
